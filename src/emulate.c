4ae15b362473653a2745167c958a48905ec8b214
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-05-25 21:50:44 +0100

    Convert all indentation to spaces

--- src/emulate.c
+++ src/emulate.c
@@ -6,10 +6,10 @@ void read_binary_file(int* mem, char* path);
 int main(int argc, char** argv) {
 
   // Check for correct number of arguments
-	if (argc != 2) {
-		printf("No file provided\n");
-		return EXIT_FAILURE;
-	}
+  if (argc != 2) {
+    printf("No file provided\n");
+    return EXIT_FAILURE;
+  }
 
   // Memory store
   int max_memory_size = (1 << 4);
@@ -17,8 +17,8 @@ int main(int argc, char** argv) {
 
   // Initialise size of memory
   for (int i = 0; i < max_memory_size; i++) {
-		memory[i] = 0;
-	}
+    memory[i] = 0;
+  }
 
   read_binary_file(memory, argv[1]);
 
@@ -32,22 +32,22 @@ int main(int argc, char** argv) {
 }
 
 void read_binary_file(int* mem, char* path) {
-	
+  
   // Open the input file
-	FILE* input = fopen(path, "rb");
+  FILE* input = fopen(path, "rb");
 
-	// Reading the input file
-	int pos = 0;
+  // Reading the input file
+  int pos = 0;
   int read_success = 0;
 
-	// Keep reading bytes into memory until nothing left to read
+  // Keep reading bytes into memory until nothing left to read
   do {
-		read_success = fread(&mem[pos], sizeof(int), 1, input);
-		pos++;
-	}
+    read_success = fread(&mem[pos], sizeof(int), 1, input);
+    pos++;
+  }
   while (read_success != 0);
 
   // Close the file
-	fclose(input);
+  fclose(input);
 
 }
