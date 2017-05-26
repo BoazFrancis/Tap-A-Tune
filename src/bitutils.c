0a803b463755ff43c7309761c9277cb9c41bd2b8
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2017-05-26 12:32:26 +0100

    Method to convert between different bases

--- src/bitutils.c
+++ src/bitutils.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <math.h>
 #include "emulate.h"
 
 /**
@@ -33,4 +35,41 @@ int extract_bits(int* n, int start, int length) {
 */
 int extract_bit(int* n, int start) {
   return extract_bits(n, start, 1);
+}
+
+/**
+ * Returns whether the bit is set (either 1 or 0)
+ * @param n - The pointer to the number
+ * @param bit - The bit to check (0 indexed)
+ * @returns 1 or 0 on success or failure
+*/
+int is_bit_set(int* n, int bit) {
+  return (*n & (1 << bit)) > 0;
+}
+
+/**
+ * Converts a binary number to decimal
+ * @param bin - The binary number
+ * @returns the unsigned decimal value
+*/
+unsigned int binary_to_decimal(unsigned int bin) {
+  return base_to_base(2, 10, bin);
+}
+
+/**
+ * Converts any number from one base to another
+ * @param from - The starting base
+ * @param to - The ending base
+ * @param n - The original number
+ * @returns the converted value
+*/
+unsigned int base_to_base(int from, int to, unsigned int n) {
+  int new_number = 0, i = 0, rem;
+  while (n != 0) {
+    rem = n % from;
+    n /= from;
+    new_number += rem*pow(to,i);
+    i++;
+  }
+  return new_number;
 }
