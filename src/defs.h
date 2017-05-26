f239721eaec3853c862cd626094d348c362f83f0
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2017-05-26 12:32:11 +0100

    Add identifying bits for instructions

--- src/defs.h
+++ src/defs.h
@@ -5,6 +5,12 @@
 // Useful registers
 #define CPSR_REGISTER 16
 
+// Identifying bits for instructions (bits 26, 27)
+#define BRANCH_IDENTIFIER 27
+#define SINGLE_DATA_IDENTIFIER 26
+#define MULT_ID_1 4
+#define MULT_ID_2 7
+
 // CPSR register
 #define CPSR_N 31
 #define CPSR_C 29
