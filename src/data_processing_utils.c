9295831fde04911bbedda58d8852e8b24c984bd5
azakon97 <azakon1997@gmail.com>
2017-05-28 19:05:48 +0100

    wrote comments to the data_processing_utils.c

--- src/data_processing_utils.c
+++ src/data_processing_utils.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include "emulate.h"
 
+/**
+ * Performs shift to the left (logical = arithmetic) on 'val'
+ * @param val - The value being shifted
+ * @param shiftBy - The number of bits shifted by
+ * @returns the value shifted left by the specified number of bits
+*/
 unsigned int shiftLeft(unsigned const int val, unsigned int shiftBy) {
   return val << shiftBy;
 }
 
+/**
+ * Performs logical shift to the right on 'val'
+ * @param val - The value being shifted
+ * @param shiftBy - The number of bits shifted by
+ * @returns the value shifted (logical) right by the specified number of bits
+*/
 int logicalShiftRight(const int val, unsigned int shiftBy) {
   return val >> shiftBy;
 }
 
-
+/**
+ * Performs arithmetic shift to the right on 'val'
+ * @param val - The value being shifted
+ * @param shiftBy - The number of bits shifted by
+ * @returns the value shifted (arithmetic) right by the specified number of bits
+*/
 int arithmeticShiftRight(const int val, unsigned int shiftBy) {
   return sign_extension(val, 32, 32 + shiftBy) >> shiftBy;
 }
@@ -31,6 +48,14 @@ signed int sign_extension(signed int n, int sbits, int ebits) {
 
 }
 
+/**
+ * Performs the shift operation according to the specified shift type on 'val'
+ * by the specified number of bits
+ * @param shiftType - shift type
+ * @param val - The value being shifted
+ * @param shiftBy - The number of bits shifted by
+ * @returns returns the shifted value
+*/
 unsigned int shift_by_type(unsigned int shiftType, unsigned int val, unsigned int shiftBy) {
   switch (shiftType) {
     // logical shift left
