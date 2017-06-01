c893b35714885d24728290d0fd69747eeef5a392
Alexandr Zakon <az1116@point33.doc.ic.ac.uk>
2017-06-01 16:57:23 +0100

    Wrote a file reader for assembler, and started writing a helper method to identify instruction

---
+++ src/identify_instruction.c
@@ -0,0 +1,24 @@
+#include <string.h>;
+
+int isDataProcessing(char *mnem) {
+  // strcmp(s1, s2) returns 0 is string s1 is the same as s2
+  return !strcmp(*mnem, add) || !strcmp(*mnem, sub) || !strcmp(*mnem, rsb) ||
+         !strcmp(*mnem, and) || !strcmp(*mnem, add) || !strcmp(*mnem, add) ||
+         !strcmp(*mnem, add) || !strcmp(*mnem, add) || !strcmp(*mnem, add) ||
+}
+
+int isMultiply(char *mnem) {
+
+}
+
+int isSDT(char * mnem) {
+
+}
+
+int isBranch(char *mnem) {
+
+}
+
+int isSpecial(char *mnem) { // if shravan
+
+}
