92e2ba741c9fdf0d71006bec96d7004a440ccd5f
Alexandr Zakon <az1116@point33.doc.ic.ac.uk>
2017-06-01 17:16:46 +0100

    worked on identify_instruction.c

--- src/identify_instruction.c
+++ src/identify_instruction.c
@@ -2,13 +2,14 @@
 
 int isDataProcessing(char *mnem) {
   // strcmp(s1, s2) returns 0 is string s1 is the same as s2
-  return !strcmp(*mnem, add) || !strcmp(*mnem, sub) || !strcmp(*mnem, rsb) ||
-         !strcmp(*mnem, and) || !strcmp(*mnem, add) || !strcmp(*mnem, add) ||
-         !strcmp(*mnem, add) || !strcmp(*mnem, add) || !strcmp(*mnem, add) ||
+  return !strcmp(*mnem, "add") || !strcmp(*mnem, "sub") || !strcmp(*mnem, "rsb") ||
+         !strcmp(*mnem, "and") || !strcmp(*mnem, "eor") || !strcmp(*mnem, "orr") ||
+         !strcmp(*mnem, "mov") || !strcmp(*mnem, "tst") || !strcmp(*mnem, "teq") ||
+         !strcmp(*mnem, "cmp");
 }
 
 int isMultiply(char *mnem) {
-
+  return !strcmp(*mnem, mul) || !strcmp(*mnem, )
 }
 
 int isSDT(char * mnem) {
