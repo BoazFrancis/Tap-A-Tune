8bc79867c8f10300c8bb845d511a82f71eadb9ef
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2017-06-04 23:17:25 +0100

    Standard mutliply instruction for assembler

--- src/assembler/assembler_utils.c
+++ src/assembler/assembler_utils.c
@@ -180,35 +180,34 @@ int do_sub(char* params) {
 
 int do_cmp(char* params) {
 
-    char* value;
-    unsigned int cond = 14 << COND_START;
-    char* reg = strtok_r(params, ",", &value);
-    value = trim_whitespace(value);
-    unsigned int instruction = 0;
-    unsigned int opcode = CMP << 21;
-    unsigned int op2;
-    set_bit(&instruction, 20);
-    if (value[0] == '#') {
-      // Immediate value
-      set_bit(&instruction, DATA_PROC_IMM_IDENTIFIER);
-      op2 = strtol(value+1, NULL, 0);
-      int rotation = get_rotated_op(&op2);
-      // The full operand is the first 8 bits of op2 | the 4 bits of rotation
-      op2 = (rotation << 8) | op2;
-    }
-    else {
-      // Register e.g. mov r2, r1
-      clear_bit(&instruction, DATA_PROC_IMM_IDENTIFIER);
-      op2 = strtol(value+1, NULL, 0);
-    }
+  char* value;
+  unsigned int cond = 14 << COND_START;
+  char* reg = strtok_r(params, ",", &value);
+  value = trim_whitespace(value);
+  unsigned int instruction = 0;
+  unsigned int opcode = CMP << 21;
+  unsigned int op2;
+  set_bit(&instruction, 20);
+  if (value[0] == '#') {
+    // Immediate value
+    set_bit(&instruction, DATA_PROC_IMM_IDENTIFIER);
+    op2 = strtol(value+1, NULL, 0);
+    int rotation = get_rotated_op(&op2);
+    // The full operand is the first 8 bits of op2 | the 4 bits of rotation
+    op2 = (rotation << 8) | op2;
+  }
+  else {
+    // Register e.g. mov r2, r1
+    clear_bit(&instruction, DATA_PROC_IMM_IDENTIFIER);
+    op2 = strtol(value+1, NULL, 0);
+  }
 
-    // Store Rd register, removing the "r"
-    long unsigned reg_binary = strtol(reg+1, NULL, 0);
-    reg_binary <<= 16;
+  // Store Rd register, removing the "r"
+  long unsigned reg_binary = strtol(reg+1, NULL, 0);
+  reg_binary <<= 16;
 
-    instruction |= cond | op2 | reg_binary | opcode;
-    return instruction;
-
+  instruction |= cond | op2 | reg_binary | opcode;
+  return instruction;
 
 }
 
@@ -289,6 +288,30 @@ int do_tst(char* params) {
 }
 
 int do_mul(char* params) {
+
+  // Declare binary instruction, set it to 0
+  unsigned int instruction = 0;
+  char* rest;
+
+  // Set the condition field to 1110 (i.e. 14)
+  unsigned int cond = 14 << COND_START;
+
+  // Set bits 4 and 7
+  // Bits A and S are not set
+  set_bit(&instruction, 4);
+  set_bit(&instruction, 7);
+
+  char* rd_string = strtok_r(params, ",", &rest);
+  char* rs_string = strtok_r(trim_whitespace(rest), ",", &rest);
+  char* rm_string = strtok_r(trim_whitespace(rest), ",", &rest);
+
+  // Set Rn to value of base register
+  unsigned int rd = strtol(rd_string+1, NULL, 0) << 16;
+  unsigned int rs = strtol(rm_string+1, NULL, 0) << 8;
+  unsigned int rm = strtol(rs_string+1, NULL, 0);
+
+  instruction |= cond | rd | rs | rm;
+  return instruction;
 
 }
 
