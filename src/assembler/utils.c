56a27ab728b1ebef38bc3f5b7584a1c0aa83958b
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2017-06-06 14:00:36 +0100

    Finish commenting utils and translation source files

--- src/assembler/utils.c
+++ src/assembler/utils.c
@@ -1,23 +1,35 @@
 #include <ctype.h>
 #include "assemble.h"
 
-char* cat(char* m, char* r, const char* e) {
+/**
+ * Concatenate 3 strings into one and return a new pointer to the string
+ * @param a - A pointer to the first string
+ * @param b - A pointer to the second string
+ * @param b - A pointer to the third string
+ * @returns the concatenated string
+ */
+char* cat(char* a, char* b, const char* c) {
 
   char* result = calloc(LENGTH_OF_LINE, sizeof(char));
-  for (int i = 0; m[i] != '\0'; i++) {
-    result[i] = m[i];
+  for (int i = 0; a[i] != '\0'; i++) {
+    result[i] = a[i];
   }
-  for (int i = 0; r[i] != '\0'; i++) {
-    result[i + strlen(m)] = r[i];
+  for (int i = 0; b[i] != '\0'; i++) {
+    result[i + strlen(a)] = b[i];
   }
-  for (int i = 0; e[i] != '\0'; i++) {
-    result[i + strlen(m) + strlen(r)] = e[i];
+  for (int i = 0; c[i] != '\0'; i++) {
+    result[i + strlen(a) + strlen(b)] = c[i];
   }
 
   return result;
 
 }
 
+/**
+ * Removes trailing and leading whitespace from string
+ * @param str - The string
+ * @returns the adapted string
+ */
 char* trim_whitespace(char *str) {
 
   char *end;
