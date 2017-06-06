f8eae43d08ea28be8e6f667703e9172273652f5b
Alexandr Zakon <azakon1997@gmail.com>
2017-06-06 16:41:54 +0100

    added comments to ldr and str

--- src/assembler/single_data_transfer.c
+++ src/assembler/single_data_transfer.c
@@ -3,13 +3,26 @@
 #define IS_LDR 1
 #define NOT_LDR 0
 
+/**
+ * Obtains ldr instruction
+ * @param params - 
+ * @param instruction_addr
+ * @param num_no_labels
+ * @param memory
+ * @returns the instruction
+*/
 int do_ldr(char* params, int instruction_addr, int* num_no_labels, int** memory) {
   do_sdt(params, instruction_addr, num_no_labels, memory, IS_LDR);
 }
 
+/**
+ * Obtains str instruction
+ * @param params
+ * @param instruction_addr
+ * @param num_no_labels
+ * @param memory
+ * @returns the instruction
+*/
 int do_str(char* params, int instruction_addr, int* num_no_labels, int** memory) {
-  // NOTE: this is the same as ldr but without the numberic constant cas
-  // for <address>, and L bit is NOT set
-
   do_sdt(params, instruction_addr, num_no_labels, memory, NOT_LDR);
 }
