8944d458f6e971547d501b8adb3a103683cfc94e
Shravan Nageswaran <sn2316@point36.doc.ic.ac.uk>
2017-06-06 13:58:43 +0100

    Wrote and finished gpio.s, then made kernel.img file

---
+++ src/part3/gpio.s
@@ -0,0 +1,30 @@
+ldr r0,=0x20200004
+
+mov r1,#0x00040000
+str r1,[r0]
+
+mov r2,#0x00010000
+
+ldr r3,=0x20200028
+
+ldr r4,=0x2020001C
+
+while:
+
+str r2,[r4]
+
+mov r5,#0x3F0000
+delay:
+sub r5,r5,#1
+cmp r5,#0xFF
+bne delay
+
+str r2,[r3]
+
+mov r5,#0x3F0000
+delay:
+sub r5,r5,#1
+cmp r5,#0xFF
+bne delay
+
+b while
