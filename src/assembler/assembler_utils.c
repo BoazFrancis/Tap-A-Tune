c6c5c6744aef62bec5953ecd2d198cdb99211190
Alexandr Zakon <az1116@point38.doc.ic.ac.uk>
2017-06-06 12:17:46 +0100

    Extended sub method to pass optional add test

--- src/assembler/assembler_utils.c
+++ src/assembler/assembler_utils.c
@@ -194,16 +194,18 @@ int do_sub(char* params) {
   unsigned int rn = strtol(rn_string+1, NULL, 0) << 12;
 
   char* rd_string = strtok_r(value, ",", &value);
-  // set Rd to value of destination register
+  // set Rd to the destination register number
   value = trim_whitespace(value);
   unsigned int rd = strtol(rd_string+1, NULL, 0) << 16;
 
   unsigned int opcode = SUB << 21;
+
+  // Declare Operand2
   unsigned int op2;
-  clear_bit(&instruction, 20);
+
 
   if (value[0] == '#') {
-    // Immediate value
+    // operand 2 is an Immediate value
     set_bit(&instruction, DATA_PROC_IMM_IDENTIFIER);
     op2 = strtol(value+1, NULL, 0);
     int rotation = get_rotated_op(&op2);
@@ -211,9 +213,66 @@ int do_sub(char* params) {
     op2 = (rotation << 8) | op2;
   }
   else {
-    // Register e.g. mov r2, r1
-    clear_bit(&instruction, DATA_PROC_IMM_IDENTIFIER);
-    op2 = strtol(value+1, NULL, 0);
+    // Operand2 is of the form of a shifted register, Rm{,<shift>}
+    
+
+    //Declare Rm string and Rm register number
+    char* rm_str;
+    unsigned int rm = 0;
+
+
+    // value now has the form of "Rm{, <shift>}"
+
+    //check for the comma - i.e. check for existence of {, <shift>}
+    char* is_comma;
+    is_comma = strpbrk(value, ",");
+    if (is_comma == NULL) {
+      // there is no comma
+      // therefore value has the form of "Rm"
+      // Obtain Rm register number
+      rm = strtol(value+1, NULL, 0);
+      // or Operand2 with Rm - leaving the shift (bits 4-11) clear
+      op2 |= rm;
+    } else {
+      // There is a comma
+      // Declare offset_str which will be used for bits (4-11)
+      char* offset_str;
+      // therefore we need to split by the comma
+
+      rm_str = strtok_r(value, ",", &offset_str);
+      // Obtain Rm register number
+      rm = strtol(rm_str+1, NULL, 0);
+
+
+      char* shift_type_str;
+      //now split by the space to get the shift type
+      shift_type_str = strtok_r(offset_str, " ", &offset_str);
+
+      // shift type is lsl by default
+      unsigned int shift_type = 0;
+      if (!strcmp(shift_type_str, "lsl")) {shift_type << 5;}
+      if (!strcmp(shift_type_str, "lsr")) {shift_type = 1 << 5;}
+      if (!strcmp(shift_type_str, "asr")) {shift_type = 2 << 5;}
+      if (!strcmp(shift_type_str, "ror")) {shift_type = 3 << 5;}
+
+      op2 |= rm | shift_type;
+      // now offset_str = shift (either register or a constant amount)
+      if (offset_str[0] == '#') {
+        // shift = constant amount
+        // leave 4th bitsr #2"
+
+        // or offset with the constant (bits 7 - 11)
+        op2 |= strtol(offset_str+1, NULL, 0) << 7;
+      } else {
+        // shift = register
+        // set 4th bit
+        set_bit(&op2, 4);
+
+        // bit 7 is zero
+        // or offset with the shift register (Rs) number
+        op2 |= strtol(offset_str+1, NULL, 0) << 8;
+      }
+    }
   }
 
   instruction |= cond | op2 | rd | rn | opcode;
