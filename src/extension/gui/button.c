b87525dc14f233f5f814755fceb9100f8bbf0c6f
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2017-06-07 12:03:46 +0100

    Added dialog for button callback

--- src/extension/gui/button.c
+++ src/extension/gui/button.c
@@ -8,13 +8,19 @@ void create_button(GtkWidget* container) {
   gtk_widget_set_size_request(button, 80, 30);
 
   // On click, call print_msg
-  g_signal_connect(button, "clicked", G_CALLBACK(print_msg), NULL);
+  g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(show_dialog), NULL);
+
+  // Mouse enters the button
+  g_signal_connect(G_OBJECT(button), "enter", G_CALLBACK(enter_button), NULL);
 
   // Set a hover tooltip
   gtk_widget_set_tooltip_text(button, "Button widget");
 
 }
 
-void print_msg(GtkWidget *widget, gpointer window) {
-  g_printf("Button clicked\n");
+
+
+void enter_button(GtkWidget *widget, gpointer data) {
+  GdkColor col = {0, 27000, 30000, 35000};
+  gtk_widget_modify_bg(widget, GTK_STATE_PRELIGHT, &col);
 }
--- src/extension/gui/gui.h
+++ src/extension/gui/gui.h
@@ -7,7 +7,9 @@
 GdkPixbuf *create_pixbuf(const gchar * filename);
 void set_favicon(GtkWidget* window);
 
-void create_button(GtkWidget* window);
-void print_msg(GtkWidget *widget, gpointer window);
+void create_button(GtkWidget* container);
+void enter_button(GtkWidget *widget, gpointer data);
 
-void add_toolbar(GtkWidget* window);
+void add_toolbar(GtkWidget* container);
+
+void show_dialog(GtkWidget *widget, gpointer window);
