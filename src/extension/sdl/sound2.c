466728310ecf19b25f88e0dcfa8cb12453e3c184
Alexandr Zakon <azakon1997@gmail.com>
2017-06-08 13:02:56 +0100

    Breaking down sound2.c

--- src/extension/sdl/sound2.c
+++ src/extension/sdl/sound2.c
@@ -4,9 +4,11 @@
 
 int main(int argc, char *argv[])
 {
-        SDL_Surface *screen;
+        //SDL_Surface *screen;
         Mix_Chunk *sound = NULL;
         int channel;
+
+        // 22050 is a good frequency value to use
         int audio_rate = 22050;
         Uint16 audio_format = AUDIO_S16SYS;
         int audio_channels = 2;
@@ -19,16 +21,21 @@ int main(int argc, char *argv[])
                 printf("Unable to initialize audio: %s\n", Mix_GetError());
                 exit(1);
         }
+
         sound = Mix_LoadWAV("sound.wav");
         if(sound == NULL) {
                 printf("Unable to load WAV file: %s\n", Mix_GetError());
         }
-        screen = SDL_SetVideoMode(320, 240, 0, SDL_ANYFORMAT);
+
+
+        /*screen = SDL_SetVideoMode(320, 240, 0, SDL_ANYFORMAT);
         if (screen == NULL) {
                 printf("Unable to set video mode: %s\n", SDL_GetError());
                 return 1;
-        }
-        channel = Mix_PlayChannel(-1, sound, 10);
+        }*/
+
+
+        channel = Mix_PlayChannel(-1, sound, 1);
         if(channel == -1) {
                 printf("Unable to play WAV file: %s\n", Mix_GetError());
         }
